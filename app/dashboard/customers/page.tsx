4822316fdb54325232df7836212e0ecf6e01cf7c
BrodyGao <gaolianbo@qq.com>
2024-05-15 14:01:08 +0800

    init

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>customers page</h1>
+    </div>
+  );
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+  return (
+    <p>This is the ivoivoces page.</p>
+  );
+}
